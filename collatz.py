ef4736f55f81371b61190b7341a30ee7634e1002
p1nwheel <richard+git@geodisc.net>
2015-10-11 11:11:02 -0500

    cleaned up collatz.py and removed references to global in functions

--- collatz.py
+++ collatz.py
@@ -1,15 +1,16 @@
 def collatz(numberIn):
-    global number
     number = numberIn
-    if number % 2 == 0:     # True if input is Even
-        number = number // 2
-        print(str(number))
-    elif number % 2 == 1:   # True if input is Odd
-        number = 3 * number + 1
-        print(str(number))
+    while True:
+        if number == 1:
+            break
+        if number % 2 == 0:     # True if input is Even
+            number = number // 2
+            print(str(number))
+        elif number % 2 == 1:   # True if input is Odd
+            number = 3 * number + 1
+            print(str(number))
 
 def enterNum():
-    global number
     while True:
         try:
             fail = 0
@@ -18,12 +19,12 @@ def enterNum():
         except ValueError:
             print('Enter whole number only!')
             fail = 1
+        except NameError:
+            print('Enter whole number only!')
+            fail = 1
         if fail == 0:
-		return(number)
-            	break
+                return(number)
+                break
 
-enterNum()
-while True:
-    collatz(number)
-    if number == 1:
-        break
+number = enterNum()
+collatz(number)
