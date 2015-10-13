4d7d04aa8779a293271742c7139893d38ebe3730
p1nwheel <richard+git@geodisc.net>
2015-10-12 19:14:24 -0500

    removed ambiguity from collatz

--- collatz.py
+++ collatz.py
@@ -1,15 +1,19 @@
 def collatz(numberIn):
-    number = numberIn
-    while not number == 1:
-        if number % 2 == 0:     # True if input is Even
-            number = number // 2
-            print(str(number))
-        elif number % 2 == 1:   # True if input is Odd
-            number = 3 * number + 1
-            print(str(number))
+    if type(numberIn) == int:
+        number = numberIn
+        while not number == 1:
+            if number % 2 == 0:     # True if input is Even
+                number = number // 2
+                print(str(number))
+            elif number % 2 == 1:   # True if input is Odd
+                number = 3 * number + 1
+                print(str(number))
+    else:
+        print('Collatz requires integer value to execute')
 
 def enterNum():
-    while True:
+    #while True:
+    for attemptsTaken in range(1, 4):
         try:
             fail = 0
             print('Enter Number:')
@@ -21,8 +25,9 @@ def enterNum():
             print('Enter whole number only!')
             fail = 1
         if fail == 0:
-                return(number)
-                break
+            return(number)
+            break
+    print('No number entered after 3 attempts')
 
 number = enterNum()
 collatz(number)
